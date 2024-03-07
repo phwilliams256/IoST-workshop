ab8516ea5546b03ac10ad9b6053fe803519504ad
phwilliams256 <phwilliams@ualr.edu>
2024-03-07 17:00:05 -0600

    First commit for get I2C scan

---
+++ i2ctesting.py
@@ -0,0 +1,7 @@
+# i2ctesting.py
+
+import board                           # Raspberry Pi Pico, Circuitpython
+from busio import I2C
+i2c = I2C(sda=board.GP4, scl=board.GP5)
+while i2c.try_lock():                  # CP requires I2C bus locking
+    pass
